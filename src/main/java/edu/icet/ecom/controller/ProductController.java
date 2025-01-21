f2610842a9654b14b47b1b73cadee4adfc268cab
seniru-dinil <senirudinil927@gmail.com>
2025-01-21 12:16:02 +0530

    dependecy injection

--- src/main/java/edu/icet/ecom/controller/ProductController.java
+++ src/main/java/edu/icet/ecom/controller/ProductController.java
@@ -2,7 +2,7 @@ package edu.icet.ecom.controller;
 
 import edu.icet.ecom.model.Product;
 import edu.icet.ecom.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,20 +10,22 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@RequiredArgsConstructor
 public class ProductController {
 
-    @Autowired
-    ProductService productService;
+
+    final ProductService productService;
+
 
     @GetMapping("/get-product")
-    ArrayList<Product> getProductList() {
+    public ArrayList<Product> getProductList() {
         ArrayList<Product> productList = productService.getProductList();
         return productList;
     }
 
-    
+
     @PostMapping("/add-product")
-    String addProduct(@RequestBody Product product){
+    public String addProduct(@RequestBody Product product) {
         return (product).toString();
     }
 }
--- src/main/java/edu/icet/ecom/service/ProductService.java
+++ src/main/java/edu/icet/ecom/service/ProductService.java
@@ -1,20 +1,12 @@
 package edu.icet.ecom.service;
 
+
 import edu.icet.ecom.model.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
-
 @Service
-public class ProductService {
-    
-        public ArrayList<Product> getProductList(){
-            ArrayList<Product> products = new ArrayList<>();
-            products.add(new Product("Banana", "Cargo", 100.0));
-            products.add(new Product("Cream Cracker", "Unileaver", 300.0));
-            products.add(new Product("Fresh Milk", "Sathosa", 300.0));
-            return  products;
-        }
-
+public interface ProductService {
+    public ArrayList<Product> getProductList();
 }
